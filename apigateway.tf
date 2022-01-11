759863e43e6833bfd43de0341a734e3d68b953b8
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2022-01-11 12:24:26 -0300

    Created API Gateway

--- apigateway.tf
+++ apigateway.tf
@@ -1,7 +1,22 @@
 # API Gateway resource
-resource "aws_api_gateway_rest_api" "APIGateway" {
+resource "aws_api_gateway_rest_api" "apigateway" {
   name        = "rsalinas-users-api"
   description = "users api"
+}
+
+# CreateUser API endpoint
+resource "aws_api_gateway_resource" "createuser" {
+  path_part   = "create"
+  parent_id   = aws_api_gateway_rest_api.apigateway.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.apigateway.id
+}
+
+# CreateUser API method
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.apigateway.id
+  resource_id   = aws_api_gateway_resource.createuser.id
+  http_method   = "POST"
+  authorization = "NONE"
 }
 
 # Provide our API Gateway with permissions to executi each Lambda function
