dd6621c14e88c3e8a4673a743e0e77ec6207182c
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2022-01-11 11:02:12 -0300

    Provided our API Gateway with permissions to execute our Lambda function

--- apigateway.tf
+++ apigateway.tf
@@ -4,3 +4,14 @@ resource "aws_api_gateway_rest_api" "APIGateway" {
   description = "users api"
 }
 
+# Provide our API Gateway with permissions to executi each Lambda function
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "rsalinas-users-create"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.MyDemoAPI.execution_arn}/*/*/*"
+}
