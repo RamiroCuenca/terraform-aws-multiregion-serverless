3c1854fa0527f98ccda67d86131293a2baa1d96a
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2022-01-12 11:24:09 -0300

    Reorganized DYNAMODB file

---
+++ dynamodb.tf
@@ -0,0 +1,13 @@
+# DynamoDB table
+resource "aws_dynamodb_table" "usersDB" {
+  name             = "rcs-serverless-users"
+  hash_key         = "id"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
