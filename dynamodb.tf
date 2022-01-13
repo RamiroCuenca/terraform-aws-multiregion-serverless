4931776a4b1fa3c890ca628a1d51fa3692459ced
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2022-01-13 11:05:00 -0300

    Setted up global table for DYNAMODB

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,5 +1,5 @@
-# DynamoDB table
-resource "aws_dynamodb_table" "usersDB" {
+# DynamoDB main table
+resource "aws_dynamodb_table" "users_virginia" {
   name             = "rcs-serverless-users"
   hash_key         = "id"
   billing_mode     = "PAY_PER_REQUEST"
@@ -9,5 +9,38 @@ resource "aws_dynamodb_table" "usersDB" {
   attribute {
     name = "id"
     type = "S"
+  }
+}
+
+# Dynamo Replica table
+resource "aws_dynamodb_table" "users_ohio" {
+  provider = aws.ohio
+
+  name             = "rcs-serverless-users"
+  hash_key         = "id"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_global_table" "users" {
+  depends_on = [
+    aws_dynamodb_table.users_virginia,
+    aws_dynamodb_table.users_ohio,
+  ]
+
+  name = "rcs-serverless-users"
+
+  replica {
+    region_name = "us-east-1"
+  }
+
+  replica {
+    region_name = "us-east-2"
   }
 }
